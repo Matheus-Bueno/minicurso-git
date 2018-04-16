c9f0c98fb8c129d3d7771ab7a3d8dd610ed3375c
Matheus-Bueno <matheusdbuenoj@outlook.com>
2018-04-16 16:19:09 -0300

    altera a seq

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
+nome =  input("Qual o seu nome?")
+
+
 a = 1
 b = 2 
 
 print("resultado:")
 print(a+b+b)
 
-
-nome =  input("Qual o seu nome?")
-print("olá",nome)
-
-
-<<<<<<< HEAD
 print("nova alteracao")
-input("")
-=======
 
 
 print("hello my friends i'am back")
+print("olá",nome)
 input("")
->>>>>>> getname
